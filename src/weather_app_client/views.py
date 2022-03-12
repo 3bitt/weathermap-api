96cecac4115f37362deb3f7e5ff570ee50c23917
Kuba T <batoniczny@gmail.com>
2022-03-12 01:09:36 +0100

    add status import

--- src/weather_app_client/views.py
+++ src/weather_app_client/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from weather_app_client.models import CityWeather
 from weather_app_client.serializers import CityWeatherSerializer
@@ -14,7 +14,7 @@ class CityWeatherView(viewsets.ViewSet):
             serializer.save()
             return Response({"result":"Weather data collected"}, status=status.HTTP_200_OK)
         return Response({"error": "something went wrong"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+    
 
     def list(self, request):
         queryset = CityWeather.objects.all()
